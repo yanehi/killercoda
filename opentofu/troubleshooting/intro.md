f7c03c84eb1e3b09b863766d35578a57db7e252c
Yannic Nevado Hidalgo <nevado-git@magenta.de>
2025-07-24 15:40:06 +0200

    feat: add solution creation for task-1

--- opentofu/troubleshooting/intro.md
+++ opentofu/troubleshooting/intro.md
@@ -1,1 +1,5 @@
-Find and solve common issues in Terraform/OpenTofu.
+# Troubleshooting in Terraform/OpenTofu
+
+Learn to identify and resolve common issues in Terraform/OpenTofu configurations.
+
+> **_NOTE:_** Please wait until the environment is fully prepared before starting the exercise (`Start` button). You can monitor the preparation status in the terminal on the right side.
---
+++ opentofu/troubleshooting/task-1/verify.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Create provider.tf file
+
+cat <<'EOF' > ~/troubleshooting/solution-1/provider.tf
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+EOF
+
+cat <<'EOF' > ~/troubleshooting/solution-1/variables.tf
+variable "environment" {
+  type        = string
+  description = "Environment to deploy the container (dev, test, prod)"
+  default     = "dev"
+}
+
+variable "nginx_image_tag" {
+  type        = string
+  description = "Image Tag for Nginx"
+  default     = "latest"
+}
+
+variable "alpine_image_tag" {
+  type        = string
+  description = "Image Tag for Alpine"
+  default     = "latest"
+}
+EOF
+
+cat <<'EOF' > ~/troubleshooting/solution-1/main.tf
+# Pull the nginx image
+resource "docker_image" "nginx" {
+  name = "${local.nginx_image_name}:${var.nginx_image_tag}"
+}
+
+# Pull the alpine image
+resource "docker_image" "alpine" {
+  name = "${local.alpine_image_name}:${var.alpine_image_tag}"
+}
+
+# Create a nginx container which depends on the creation of the alpine container
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = local.nginx_container_name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+# Create an alpine container
+resource "docker_container" "alpine" {
+  image = docker_image.alpine.name
+  name  = local.alpine_container_name
+
+  # Keep the container running
+  command = ["tail", "-f", "/dev/null"]
+}
+EOF
+
+
+cat <<'EOF' > ~/troubleshooting/solution-1/locals.tf
+locals {
+  alpine_image_name = "alpine"
+  nginx_image_name  = "nginx"
+
+  alpine_container_name = "topic4-alpine-${var.environment}"
+  nginx_container_name  = "topic4-nginx-${var.environment}"
+}
+EOF
