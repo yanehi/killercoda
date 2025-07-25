babea70abc8a9ea82348d468fe3363827e29be18
Yannic Nevado Hidalgo <nevado-git@magenta.de>
2025-07-25 13:50:41 +0200

    fix: add missing folder

--- opentofu/troubleshooting/startup.sh
+++ opentofu/troubleshooting/startup.sh
@@ -2,7 +2,7 @@
 
 # Create folder with exercises
 mkdir ~/troubleshooting
-for i in {1..8}; do
+for i in {1..9}; do
   echo "Create folder task-$i and solution-$i"
   mkdir ~/troubleshooting/task-$i
   mkdir ~/troubleshooting/solution-$i
